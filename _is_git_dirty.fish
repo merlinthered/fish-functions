0dd589418280c81a71d5401a59b7b330d3a70e15
merlinthered <merlinthered@github.com>
2013-03-26 15:52:01 +0100

    Make _is_git_dirty ignore untracked files

--- _is_git_dirty.fish
+++ _is_git_dirty.fish
@@ -1,7 +1,7 @@
 #! fish
 
 function _is_git_dirty  --description "exits with status 0 if the current git working copy is dirty"
-	set st (git status -s --ignore-submodules=dirty ^/dev/null)
+	set st (git status -s --ignore-submodules=dirty --untracked-files=no ^/dev/null)
 	if test "$st"
 		return 0
 	else
