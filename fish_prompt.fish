3653c2e90f06c1c6510a2320a0b6c9e83835034c
merlinthered <theredmerlin@gmail.com>
2013-09-17 23:31:06 +0200

    More minimalistic prompt with less colors

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -3,46 +3,42 @@
 function fish_prompt
 
 	# Define colors
-	set -l cyan (set_color -o cyan)
-	set -l yellow (set_color -o yellow)
-	set -l red (set_color -o red)
-	set -l blue (set_color -o blue)
-	set -l green (set_color -o green)
+	set -l cyan (set_color cyan)
+	set -l yellow (set_color yellow)
+	set -l red (set_color red)
+	set -l blue (set_color blue)
+	set -l green (set_color green)
     set -l bold (set_color -o)
 
-	set -l cwdcolor (set_color -o $fish_color_cwd)
-	set -l dirtycolor (set_color -o red)
-	set -l cleancolor (set_color -o green)
+	set -l cwdcolor (set_color $fish_color_cwd)
+	set -l dirtycolor (set_color red)
+	set -l cleancolor (set_color green)
 
 	set -l normal (set_color normal)
 
-	set -l arrow "$red➜ $normal"
-	set -l cwd $cwdcolor(prompt_pwd)
+	set -l arrow "➜ "
+	set -l cwd (prompt_pwd)/
 
 	# Git info
 	if type git >/dev/null
-		set git_info (_git_info_string $blue" git:($normal$bold" "$blue)$normal")
+		set git_info (_git_info_string " git:(" ")")
 	else 
 		set git_info ""
 	end
 
 	# Mercurial info
 	if type hg >/dev/null
-    	set hg_info (_hg_info_string $blue" hg:($normal$bold" "$blue)$normal")
+    	set hg_info (_hg_info_string " hg:(" ")")
     else
     	set hg_info ""
     end
 
 	# Subversion info
 	if type svn >/dev/null
-    	set svn_info (_svn_info_string $blue" svn:($normal$bold" "$blue)$normal")
+    	set svn_info (_svn_info_string " svn:(" ")")
     else
     	set svn_info ""
     end
 
-    set subdirs (_count_subdirs .)
-    set files (_count_files .)
-    set dir_info $blue " (" $subdirs "/" $normal $files $blue ")" $normal
-
-	echo -n -s $arrow $cwd $git_info $hg_info $svn_info $dir_info $normal $bold '>' $normal
+	echo -n -s $arrow $cwd $git_info $hg_info $svn_info '> '
 end
